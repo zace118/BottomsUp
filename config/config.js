770a2c96b1009ffeb9312e02131f0ad31a8a9839
Zac Eskridge <zace118@gmail.com>
2020-03-10 21:00:12 -0700

    Updating Zac branch

---
+++ config/config.js
@@ -0,0 +1,25 @@
+// // require("dotenv").config();
+
+// {
+//     "development": {
+//       "username": "root",
+//       "password": "root",
+//       "database": "bottomsup",
+//       "host": "127.0.0.1",
+//       "dialect": "mysql"
+//     },
+//     "test": {
+//       "username": "root",
+//       "password": null,
+//       "database": "database_test",
+//       "host": "127.0.0.1",
+//       "dialect": "mysql"
+//     },
+//     "production": {
+//       "username": "root",
+//       "password": null,
+//       "database": "database_production",
+//       "host": "127.0.0.1",
+//       "dialect": "mysql"
+//     }
+//   }
