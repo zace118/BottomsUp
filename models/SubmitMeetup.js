e0f89abc61a1ff8938040f89fa8725895efd2880
Chris Behrens <chrisbehrens@yahoo.com>
2020-03-15 10:47:26 -0700

    added code to only append meetups that are current or in the future

--- models/SubmitMeetup.js
+++ models/SubmitMeetup.js
@@ -8,7 +8,7 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [8, 8]
+                len: [10, 10]
             }
         },
         time: {
--- public/js/meetup.js
+++ public/js/meetup.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
     $.get("/api/post_meetup", function (meetups) {
-        console.log(meetups);
+        if(meetups.length == 0) { 
+                const parentDiv = $("<div>There are no current Meetups</div>");
+                
+                parentDiv
+                    .addClass("card")
+                
+                $(".leftcolumn").append(parentDiv);
 
+            
+        
+        }
+
+        else {
         // This if statement will only post the most recent 10
-        if (meetups.length < 10) {
-            for (let i = meetups.length - 1; i >= 0; i--) {
-                console.log("if console.log: " + meetups.length);
+        for (let i = meetups.length - 1; i >= 0; i--) {
+            if (meetups[i].date >= moment().format('L')) {
+                
                 let whereData = meetups[i].location;
                 let dataDate = meetups[i].date;
                 let dataTime = meetups[i].time;
@@ -58,60 +69,22 @@ $(document).ready(function () {
                 $("#who" + i).append(whoData);
 
             }
-        }
+        
         // If there are more than 10 objects in the meetups array, it will only post the most recent 10
         else {
-            for (let i = meetups.length - 1; i > meetups.length - 11; i--) {
-                console.log("else console.log: " + meetups.length);
-                let whereData = meetups[i].location;
-                let dataDate = meetups[i].date;
-                let dataTime = meetups[i].time;
-                let whenData = `${dataDate}, ${dataTime}`;
-                let whoData = meetups[i].author;
-                let whatData = meetups[i].message;
-
-                //Generates the div tag to begin creating the cards.
-                const parentDiv = $("<div>");
-                // Adds class and ID to parentDiv
+           
+                
+                const parentDiv = $("<div>There are no current Meetups");
+                
                 parentDiv
                     .addClass("card")
-                    .attr("id", "meetup" + i);
-                // .attr("style", "width:13rem;");
-
-                // Generates the p tag for the message
-                const whatGen = $("<h3><b>What: </b></h3> ");
-                // Adds #what
-                whatGen.attr("id", "what" + i);
-
-                // Generates the h5 tag for the date and time
-                const whenGen = $("<h3><b>When: </b></h3> ");
-                //Adds #when
-                whenGen.attr("id", "when" + i);
-
-                // Generates the h2 tag for the location
-                const whereGen = $("<h3><b>Where: </b></h3>");
-                //Adds #where
-                whereGen.attr("id", "where" + i);
-
-                // Generates the p tag for the author
-                const whoGen = $("<h3><b>Posted by: </b></h3> ");
-                // Adds #who
-                whoGen.attr("id", "who" + i);
-
-
-                // Creating the card dynamically
-                $(parentDiv).append(whatGen).append(whereGen).append(whenGen).append(whoGen);
-
-                // Appending the cards to the .leftcolumn in the html
+                
                 $(".leftcolumn").append(parentDiv);
 
-                // Pasting all the info onto the cards, dynamically
-                $("#where" + i).append(whereData);
-                $("#when" + i).append(whenData);
-                $("#what" + i).append(whatData);
-                $("#who" + i).append(whoData);
-            }
+            
         }
+        }
+    }
     });
 
 
